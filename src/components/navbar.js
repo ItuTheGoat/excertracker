ba03f66126e04125aca97881817f6f0c43d76fbb
ItuDaGoat <itumelengmochabe@gmail.com>
2020-07-13 19:26:30 +0200

    Working on the navbar, Will use the legit Navbar imports

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
 
 class Navbar extends Component {
   render() {
@@ -8,19 +9,29 @@ class Navbar extends Component {
         <Link to="/" className="navbar-brand">
           ExcerTracker
         </Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="navbar-item">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
               <Link to="/" className="nav-link">
                 Exercises
               </Link>
             </li>
-            <li className="navbar-item">
+            <li className="nav-item">
               <Link to="/create" className="nav-link">
                 Create Exercise
               </Link>
             </li>
-            <li className="navbar-item">
+            <li className="nav-item">
               <Link to="/user" className="nav-link">
                 Create User
               </Link>
